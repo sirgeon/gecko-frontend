49f3e4a9a983ab316644a2a1b9b933d7ee388f33
vegewadev <hegewaldvin@gmail.com>
2024-06-15 22:35:27 +0200

    fix: typo

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -13,7 +13,7 @@ const ibm_plex_mono = IBM_Plex_Mono({
 export const metadata: Metadata = {
   title: "Kernel the Leopardgecko",
   description:
-    "Kernel is a realitvely young Leopardgecko. He arrived on the 15th June 2024. On this site you can see a lot about him including temperature, humidity and some cute pictures of that little fella.",
+    "Kernel is a relatively young Leopardgecko. He arrived on the 15th June 2024. On this site you can see a lot about him including temperature, humidity and some cute pictures of that little fella.",
 };
 
 export default function RootLayout({
